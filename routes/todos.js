7526bd7e5f62408df8c1272775210f917bdeeca1
Bambang Witoradyo <bwitoradyo@studiobits.com>
2016-10-15 15:26:45 -0400

    implement get for all and one id

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,30 @@
 var express = require ('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://bambang:bambang@ds041939.mlab.com:41939/meantodosapp', ['todos'])
 
+//Get All Todos
 router.get('/todos', function (req, res, next) {
-    res.send('TODOS API');
+    db.todos.find(function (err, todos) {
+        if(err){
+            res.send(err);
+        }else{
+           res.json(todos);
+        }
+    });
+});
+
+//Get Single Todo
+router.get('/todo/:id', function (req, res, next) {
+    db.todos.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    },function (err, todo) {
+        if(err){
+            res.send(err);
+        }else{
+            res.json(todo);
+        }
+    });
 });
 
 module.exports = router;
