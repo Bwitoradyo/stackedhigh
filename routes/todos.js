e24356d218ecd2819abc53c5f6e1559e9c645690
Bambang Witoradyo <bwitoradyo@studiobits.com>
2016-10-15 17:24:50 -0400

    implement crud

--- routes/todos.js
+++ routes/todos.js
@@ -27,5 +27,66 @@ router.get('/todo/:id', function (req, res, next) {
     });
 });
 
+//Save Todo
+router.post('/todo', function (req, res, next) {
+    var todo = req.body;
+    if (!todo.text || !(todo.isCompleted + '')){
+        res.status(400);
+        res.json({
+           "error":"Invalid Data"
+        });
+    }else {
+        db.todos.save(todo, function (err, result) {
+            if(err){
+                res.send(err);
+            }else{
+                res.json(result);
+            }
+        });
+    }
+});
+
+//Update Todo
+router.put('/todo/:id', function (req, res, next) {
+    var todo = req.body;
+    var updObj = {};
+    if(todo.isCompleted){
+        updObj.isCompleted = todo.isCompleted;
+    }
+    if(todo.text){
+        updObj.text = todo.text;
+    }
+    if(!updObj){
+        res.status(400);
+        res.json({
+            "error":"Invalid Data"
+        })
+    }else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updObj, {}, function (err, result) {
+            if(err){
+                res.send(err);
+            }else{
+                res.json(result);
+            }
+        });
+    }
+
+});
+
+//Delete Todo
+router.delete('/todo/:id', function (req, res, next) {
+        db.todos.remove({
+            _id: mongojs.ObjectId(req.params.id)
+        }, '', function (err, result) {
+            if(err){
+                res.send(err);
+            }else{
+                res.json(result);
+            }
+        });
+});
+
 module.exports = router;
 
